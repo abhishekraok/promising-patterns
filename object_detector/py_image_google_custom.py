4a43186b0cf9f253a72b24bdabf8f191690ad968
unknown <AKR156@EEW11406.coeaccess.psu.edu>
2015-01-30 13:46:28 -0500

    Modified the file, cleaned it up

--- object_detector/py_image_google_custom.py
+++ object_detector/py_image_google_custom.py
@@ -1,8 +1,9 @@
+""" File to download bunch of images from a given list of words.
+Each word (label) will be downloaded to its own folder"""
 import urllib2
 import urllib
 import simplejson
 import os
-import cStringIO
 import time
 
 def download_image_list(urllist, label='default'):
@@ -24,18 +25,24 @@ def download_image_list(urllist, label='default'):
         if not os.path.isfile(filename):
             urllib.urlretrieve(url, filename)
 
-label = 'dog'
-number_of_images = 100
-fetcher = urllib2.build_opener()
-startIndex = 0
-while startIndex < number_of_images:
-    searchUrl = "http://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=" + \
-        label + "&start=" + str(startIndex)
-    f = fetcher.open(searchUrl)
-    results = simplejson.load(f)
-    if results['responseData'] == None:
-        break
-    urllist = [i['unescapedUrl'] for i in results['responseData']['results']]
-    download_image_list(urllist, label)
-    startIndex += len(urllist)
-    time.sleep(2)
+def download_label(label):
+    """Given a search term (label) downloads it from internet"""
+    number_of_images = 100
+    fetcher = urllib2.build_opener()
+    startIndex = 0
+    while startIndex < number_of_images:
+        searchUrl = "http://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=" + \
+            label + "&start=" + str(startIndex)
+        f = fetcher.open(searchUrl)
+        results = simplejson.load(f)
+        if results['responseData'] == None:
+            break
+        urllist = [i['unescapedUrl'] for i in results['responseData']['results']]
+        download_image_list(urllist, label)
+        startIndex += len(urllist)
+        time.sleep(2)
+
+if __name__ == '__main__':
+    labels = ['dog','cat']
+    for single_label in labels:
+        download_label(single_label)
