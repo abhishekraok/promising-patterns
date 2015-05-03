ca43faced283deb25049e363ea1f9aa18d3994b0
Abhishek Rao <abhishekrao5002@gmail.com>
2015-05-03 17:15:21 -0400

    basic version of Kadnet

---
+++ object_detector/kadnet.py
@@ -0,0 +1,51 @@
+nc_labels = []  # a list of string, where each string is a label of new class node
+nc_list = []  # a list of NewClass nodes
+top_N  = 5  # number of features to select for each new class
+
+class NewClass():
+    """ A new class, a new label created from function of old"""
+    def __init__(self, label=None, output=None):
+        self.label = label if not label is None else None
+        self.output= output if not output is None else None
+        inputs = []
+        weights = []
+        
+    def get_activation(self):
+        if self.output is not None:
+            return self.output
+        else:
+            output = sum((get_activation_by_label(inputs[i]) * weights[i] for i in range(len(inputs))])
+
+def activate_all(in_net, filename, new_classes):
+    """ Activate all the nodes for given file with name
+    filename."""
+    feat7, feat8 = extract_caffe_features(in_net, filename)
+    feat8_nc = [NewClass(caffe_labels[i],feat8)] 
+    nc_activations = [nc_node.get_activation() for nc_node in new_classes]
+    all_activated_nodes =  feat8_nc + nc_activations
+    return all_activated_nodes
+
+def get_all_activation_numbers(all_activated_nodes):
+    """ Get the activation values of all nodes"""
+    all_activations_numbers = [i.output for i in all_activated_nodes]
+    return all_activations_numbers
+
+def create_new(activations_list, new_label):
+    """ pick the highest activated features and create new class"""
+    top_activations_index = np.array(activations_list).argsort()[-1:-top_N:-1]
+    new_node = NewClass(new_label)
+    
+def get_activation_by_label(node_label):
+    """ Get the activation function of the current node"""
+    
+    if node_label in caffe_labels:
+        caffe_index = caffe_labels.index(node_label)
+        return feat8[caffe_index]
+    else:
+        nc_node = nc_list[nc_labels.index(node_label)]
+        return nc_node.get_activation()
+    
+    
+
+
+
