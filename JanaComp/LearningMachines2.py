a5898da552911e6ac6178a7b4d805486914ef10f
Abhishek Rao <abhishekrao5002@gmail.com>
2015-05-20 11:17:53 -0400

    Simple classifiers bank. Saves and loads the object using pickle. Works as expected.

--- JanaComp/LearningMachines2.py
+++ JanaComp/LearningMachines2.py
@@ -7,9 +7,9 @@ Update. 19 May 2015. Let me stept this up. Instead of having a fixed width,
 TODO: let me keep expanding the width. Only the
 input width is fixed.
 """
-from docutils.nodes import classifier
 
 __author__ = 'Abhishek Rao'
+
 
 # Headers
 import numpy as np
@@ -18,15 +18,15 @@ import math
 import matplotlib.pyplot as plt
 import pickle
 import os.path
+from sklearn.metrics import accuracy_score
+
 
 # Constants
+
 
 # Global functions
 def sigmoid(x):
   return 1 / (1 + math.exp(-x))
-
-def classifier_load(filename='DefaultClassifier.pkl'):
-    pickle.load(filename)
 
 
 # Classes
@@ -59,7 +59,7 @@ class SimpleClassifierBank:
     """ A machine which stores both input X and the current output of bunch of classifiers.
     API should be similar to scikit learn"""
 
-    def __init__(self, max_width, input_width, heigh):
+    def __init__(self, max_width, input_width, height):
         """
         Initialize this class.
 
@@ -79,6 +79,7 @@ class SimpleClassifierBank:
         """Give out what it thinks from the input. Input X should be 2 dimensional.
 
         :param: X: input, dimensionn 2, (samples x dimension)"""
+        self.current_working_memory *= 0  # Flush the current input
         X = np.array(X)
         input_number_samples, input_feature_dimension = X.shape
         if len(X.shape) is not 2:
@@ -88,9 +89,10 @@ class SimpleClassifierBank:
         for classifier_i in self.classifiers_list:
             self.current_working_memory[:,classifier_i.out_address] = classifier_i.predict(self.current_working_memory)
         # need to return the rightmost nonzero column.
-        for column_j in range(self.current_working_memory.shape[1]): # traverse through columns
+        for column_j in range(self.current_working_memory.shape[1])[::-1]: # reverse traverse through columns
             if np.any(self.current_working_memory[:input_number_samples,column_j]):
-                return self.current_working_memory[:input_number_samples,column_j]
+                soft_dec = self.current_working_memory[:input_number_samples,column_j]
+                return  np.array(soft_dec > 0.5, dtype=np.int16)
         print 'Cant find any nonzero column'
         return self.current_working_memory[:,0]
 
@@ -125,23 +127,42 @@ class SimpleClassifierBank:
     def status(self):
         """Gives out the current status, like number of classifier and prints their values"""
         print 'Currently there are ', len(self.classifiers_list), ' classifiers. They are'
-        for classifier_i in self.classifiers_list:
-            print 'Classifier: ', classifier_i
-            print 'Classifier name: ', classifier_i.label
-            print 'Out address', classifier_i.out_address
-            print 'In address', classifier_i.end_in_address
-            print 'Coefficients: ', classifier_i.classifier.coef_, classifier_i.classifier.intercept_
+        classifiers_coefficients = np.zeros(self.current_working_memory.shape)
+        print 'The classifiers in this are',[classifier_i .label for classifier_i in self.classifiers_list]
+        for count, classifier_i in enumerate(self.classifiers_list):
+            coeffs_i = classifier_i.classifier.raw_coef_
+            classifiers_coefficients[count, :coeffs_i.shape[1]] = coeffs_i
+        #    print 'Classifier: ', classifier_i
+        #    print 'Classifier name: ', classifier_i.label
+        #    print 'Out address', classifier_i.out_address
+        #    print 'In address', classifier_i.end_in_address
+            #print 'Coefficients: ', classifier_i.classifier.coef_, classifier_i.classifier.intercept_
         plt.imshow(self.current_working_memory,interpolation='none',cmap='gray')
         plt.title('Current working memory')
+        plt.figure()
+        plt.imshow(classifiers_coefficients, interpolation='none', cmap='gray')
+        plt.title('Classifier coefficients')
         plt.show()
 
-if __name__ == '__main__':
-    classifier_file_name = 'ClassifierFile.pkl'
-    if os.path.isfile(classifier_file_name):
-        Main_C1 = pickle.load(open(classifier_file_name,'r'))
-    else:
-        Main_C1 = SimpleClassifierBank(max_width=200,input_width=150,height=200)
+    def score(self, X, y):
+        """
+        Gives the accuracy between predicted(X) and y
+        :param X: 2d matrix, samples X dimension
+        :param y: actual label
+        :return: float, between 0 to 1
+        """
+        yp = self.predict(X)
+        return accuracy_score(y, y_pred=yp)
 
+    def generic_task(self, X, y, task_name):
+        """
+        A generic framework to train on different tasks.
+        """
+        self.fit(X,y,task_name=task_name)
+        print 'The score for task ',task_name, ' is ', self.score(X,y)
+
+
+def task_and(classifier):
     # Task 1 noisy and
     noise_columns = np.random.randn(90, 3)
     data_columns = np.array([[1,0], [0,1], [1,1], [0,0]]*2 + [[0,1]])
@@ -150,11 +171,64 @@ if __name__ == '__main__':
     print X
     y = np.array([[0, 0,1,0]*2 + [0]])
     y_big = np.hstack([y]*10).flatten()
-    Main_C1.fit(X,y_big,task_name='Noisy and long')
-    Main_C1.status()
-    yp = Main_C1.predict(X)
+    classifier.fit(X,y_big,task_name='Noisy and long')
+    yp =classifier.predict(X)
     print 'Predicted value is '
     print yp
+    print 'Score is ',classifier.score(X,y_big)
 
+def task_XOR_problem(classifier):
+    """
+    Trains the classifier in the art of XOR problem
+    :param classifier: any general classifier.
+    :return: None
+    """
+    X = np.array([
+                     [1,0],
+                     [0,1],
+                     [1,1],
+                     [0,0]]*50)
+    y = np.array([1,1,0,0]*50)
+    classifier.fit(X,y,task_name='XOR task')
+    yp =classifier.predict(X)
+    print 'Predicted value is '
+    print yp
+    print 'Score is ',classifier.score(X,y)
+
+def task_OR_problem(classifier):
+    """
+    Trains the classifier in the art of XOR problem
+    :param classifier: any general classifier.
+    :return: None
+    """
+    X = np.array([
+                     [1,0],
+                     [0,1],
+                     [1,1],
+                     [0,0]]*50)
+    y = np.array([1,1,1,0]*50)
+    classifier.fit(X,y,task_name='OR task')
+    yp =classifier.predict(X)
+    print 'Predicted value is '
+    print yp
+    print 'Score is ',classifier.score(X,y)
+
+# Error can't do this, as this is multi class. Later will add support
+def scikit_learn_dataset_training(classifier):
+    from sklearn import datasets
+    iris = datasets.load_iris()
+    classifier.generic_task(iris.data,iris.target,'Iris')
+
+if __name__ == '__main__':
+    classifier_file_name = 'ClassifierFile.pkl'
+    if os.path.isfile(classifier_file_name):
+        Main_C1 = pickle.load(open(classifier_file_name,'r'))
+    else:
+        Main_C1 = SimpleClassifierBank(max_width=200,input_width=150,height=200)
+    #task_and(Main_C1)
+    Main_C1.status()
+    #task_XOR_problem(Main_C1)
+    #task_OR_problem(Main_C1)
     # save the classifier
     pickle.dump(Main_C1,open(classifier_file_name,'w'))
+
