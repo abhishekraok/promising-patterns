e23ba17d79671bd4c320421d67b598c56d4f5f49
Abhishek Rao <abhishekrao5002@gmail.com>
2015-05-21 12:17:08 -0400

    Adding other files too. Last commit was supposed to say Added funcitons to train the classifiers. Training tasksk like AND, OR, XOR.

--- JanaComp/LearningMachines2.py
+++ JanaComp/LearningMachines2.py
@@ -28,18 +28,20 @@ from sklearn.metrics import accuracy_score
 
 
 # Global functions
-def sigmoid(x):
-  return 1 / (1 + math.exp(-x))
+# Reason for having 10 sigmoid is to get sharper distinction.
+def sigmoid_10(x):
+    return 1 / (1 + math.exp(-10*x))
 
 
 # Classes
 class ClassifierNode:
     """ A node that contains classifier, it's input address and output address.
     """
+
     def __init__(self, end_in_address, out_address, classifier_name='Default',
-                  given_predictor=None):
+                 given_predictor=None):
         self.out_address = out_address
-        self.end_in_address = end_in_address # end column
+        self.end_in_address = end_in_address  # end column
         self.label = classifier_name  # The name of this concept. e.g. like apple etc.
         # Check whether to create a standard classifier or a custom, given one.
         if given_predictor:
@@ -65,7 +67,7 @@ class ClassifierNode:
         else:
             dec_fx = self.given_predictor(new_x)
         # Convert it into mapping between 0 to 1 instead of -1 to 1
-        return np.array([sigmoid(i) for i in dec_fx])
+        return np.array([sigmoid_10(i) for i in dec_fx])
 
 
 class SimpleClassifierBank:
@@ -83,7 +85,7 @@ class SimpleClassifierBank:
         :param height: maximum number of input samples
         :return: None
         """
-        self.current_working_memory = np.zeros([height,max_width])
+        self.current_working_memory = np.zeros([height, max_width])
         self.classifiers_out_address_start = input_width  # the start of classifiers output.
         self.classifiers_current_count = 0  # starting address for output for new classifier
         self.classifiers_list = []
@@ -91,7 +93,7 @@ class SimpleClassifierBank:
     def predict(self, X):
         """Give out what it thinks from the input. Input X should be 2 dimensional.
 
-        :param: X: input, dimensionn 2, (samples x dimension)"""
+        :param: X: input, dimension 2, (samples x dimension)"""
         self.current_working_memory *= 0  # Flush the current input
         X = np.array(X)
         input_number_samples, input_feature_dimension = X.shape
@@ -103,22 +105,22 @@ class SimpleClassifierBank:
             predicted_value = classifier_i.predict(self.current_working_memory)
             predicted_shape = predicted_value.shape
             if len(predicted_shape) < 2:
-                predicted_value = predicted_value.reshape(-1,1)
+                predicted_value = predicted_value.reshape(-1, 1)
             predicted_shape = predicted_value.shape
-            self.current_working_memory[:predicted_shape[0],classifier_i.out_address] = predicted_value
+            self.current_working_memory[:predicted_shape[0], classifier_i.out_address] = predicted_value
         # need to return the rightmost nonzero column.
-        for column_j in range(self.current_working_memory.shape[1])[::-1]: # reverse traverse through columns
-            if np.any(self.current_working_memory[:input_number_samples,column_j]):
-                soft_dec = self.current_working_memory[:input_number_samples,column_j]
-                return  np.array(soft_dec > 0.5, dtype=np.int16)
+        for column_j in range(self.current_working_memory.shape[1])[::-1]:  # reverse traverse through columns
+            if np.any(self.current_working_memory[:input_number_samples, column_j]):
+                soft_dec = self.current_working_memory[:input_number_samples, column_j]
+                return np.array(soft_dec > 0.5, dtype=np.int16)
         print 'Cant find any nonzero column'
-        return self.current_working_memory[:,0]
+        return self.current_working_memory[:, 0]
 
-    def fit(self, X, y, task_name='Default'):
+    def fit(self, x_in, y, task_name='Default'):
         """
         Adds a new classifier and trains it, similar to Scikit API
 
-        :param X: 2d Input data
+        :param x_in: 2d Input data
         :param y:  labels
         :return: None
         """
@@ -127,12 +129,12 @@ class SimpleClassifierBank:
                 > self.current_working_memory.shape[1]:
             print 'No more space for classifier. ERROR'
             raise MemoryError
-        X = np.array(X)
-        input_number_samples, input_feature_dimension = X.shape
-        if len(X.shape) is not 2:
+        x_in = np.array(x_in)
+        input_number_samples, input_feature_dimension = x_in.shape
+        if len(x_in.shape) is not 2:
             print "Error in predict. Input dimension should be 2"
             raise ValueError
-        self.current_working_memory[:X.shape[0], :X.shape[1]] = X
+        self.current_working_memory[:x_in.shape[0], :x_in.shape[1]] = x_in
         # Procure a new classifier, this might be wasteful, later perhaps reuse classifier
         # instead of lavishly getting new ones, chinese restaurant?
         new_classifier = ClassifierNode(
@@ -167,17 +169,17 @@ class SimpleClassifierBank:
         """Gives out the current status, like number of classifier and prints their values"""
         print 'Currently there are ', len(self.classifiers_list), ' classifiers. They are'
         classifiers_coefficients = np.zeros(self.current_working_memory.shape)
-        print [classifier_i .label for classifier_i in self.classifiers_list]
+        print [classifier_i.label for classifier_i in self.classifiers_list]
         for count, classifier_i in enumerate(self.classifiers_list):
-            coeffs_i = classifier_i.classifier.raw_coef_ \
-                if classifier_i.classifier_type == 'standard' else np.zeros([1,1])
+            coeffs_i = classifier_i.classifier.coef_ \
+                if classifier_i.classifier_type == 'standard' else np.zeros([1, 1])
             classifiers_coefficients[count, :coeffs_i.shape[1]] = coeffs_i
-        #    print 'Classifier: ', classifier_i
-        #    print 'Classifier name: ', classifier_i.label
-        #    print 'Out address', classifier_i.out_address
-        #    print 'In address', classifier_i.end_in_address
-            #print 'Coefficients: ', classifier_i.classifier.coef_, classifier_i.classifier.intercept_
-        plt.imshow(self.current_working_memory,interpolation='none',cmap='gray')
+            #    print 'Classifier: ', classifier_i
+            #    print 'Classifier name: ', classifier_i.label
+            #    print 'Out address', classifier_i.out_address
+            #    print 'In address', classifier_i.end_in_address
+            # print 'Coefficients: ', classifier_i.classifier.coef_, classifier_i.classifier.intercept_
+        plt.imshow(self.current_working_memory, interpolation='none', cmap='gray')
         plt.title('Current working memory')
         plt.figure()
         plt.imshow(classifiers_coefficients, interpolation='none', cmap='gray')
@@ -200,39 +202,39 @@ class SimpleClassifierBank:
         print 'Classifier was removed. Its nae was', classifier_name
         return removing_index
 
-
-    def score(self, X, y):
+    def score(self, x_in, y):
         """
-        Gives the accuracy between predicted(X) and y
-        :param X: 2d matrix, samples X dimension
+        Gives the accuracy between predicted( x_in) and y
+        :param x_in: 2d matrix, samples x_in dimension
         :param y: actual label
         :return: float, between 0 to 1
         """
-        yp = self.predict(X)
+        yp = self.predict(x_in)
         return accuracy_score(y, y_pred=yp)
 
-    def generic_task(self, X, y, task_name):
+    def generic_task(self, x_in, y, task_name):
         """
         A generic framework to train on different tasks.
         """
-        self.fit(X,y,task_name=task_name)
-        print 'The score for task ',task_name, ' is ', self.score(X,y)
+        self.fit(x_in, y, task_name=task_name)
+        print 'The score for task ', task_name, ' is ', self.score(x_in, y)
 
 
 def task_and(classifier):
     # Task 1 noisy and
     noise_columns = np.random.randn(90, 3)
-    data_columns = np.array([[1,0], [0,1], [1,1], [0,0]]*2 + [[0,1]])
-    data_columns_big = np.vstack([data_columns]*10)
-    X = np.hstack([noise_columns, data_columns_big]) # total data
+    data_columns = np.array([[1, 0], [0, 1], [1, 1], [0, 0]] * 2 + [[0, 1]])
+    data_columns_big = np.vstack([data_columns] * 10)
+    X = np.hstack([data_columns_big, noise_columns])  # total data
     print X
-    y = np.array([[0, 0,1,0]*2 + [0]])
-    y_big = np.hstack([y]*10).flatten()
-    classifier.fit(X,y_big,task_name='Noisy and long')
-    yp =classifier.predict(X)
+    y = np.array([[0, 0, 1, 0] * 2 + [0]])
+    y_big = np.hstack([y] * 10).flatten()
+    classifier.fit(X, y_big, task_name='Noisy and long')
+    yp = classifier.predict(X)
     print 'Predicted value is '
     print yp
-    print 'Score for task Noisy and long is ',classifier.score(X,y_big)
+    print 'Score for task Noisy and long is ', classifier.score(X, y_big)
+
 
 def task_XOR_problem(classifier):
     """
@@ -240,17 +242,17 @@ def task_XOR_problem(classifier):
     :param classifier: any general classifier.
     :return: None
     """
-    X = np.array([
-                     [1,0],
-                     [0,1],
-                     [1,1],
-                     [0,0]]*50)
-    y = np.array([1,1,0,0]*50)
-    classifier.fit(X,y,task_name='XOR task')
-    yp =classifier.predict(X)
+    X = np.array([[1, 0],
+                  [0, 1],
+                  [1, 1],
+                  [0, 0]] * 50)
+    y = np.array([1, 1, 0, 0] * 50)
+    classifier.fit(X, y, task_name='XOR task')
+    yp = classifier.predict(X)
     print 'Predicted value is '
     print yp
-    print 'Score for XOR problem is ',classifier.score(X,y)
+    print 'Score for XOR problem is ', classifier.score(X, y)
+
 
 def task_OR_problem(classifier):
     """
@@ -259,22 +261,25 @@ def task_OR_problem(classifier):
     :return: None
     """
     X = np.array([
-                     [1,0],
-                     [0,1],
-                     [1,1],
-                     [0,0]]*50)
-    y = np.array([1,1,1,0]*50)
-    classifier.fit(X,y,task_name='OR task')
-    yp =classifier.predict(X)
+                     [1, 0],
+                     [0, 1],
+                     [1, 1],
+                     [0, 0]] * 50)
+    y = np.array([1, 1, 1, 0] * 50)
+    classifier.fit(X, y, task_name='OR task')
+    yp = classifier.predict(X)
     print 'Predicted value is '
     print yp
-    print 'Score for OR problem is ',classifier.score(X,y)
+    print 'Score for OR problem is ', classifier.score(X, y)
+
 
 # Error can't do this, as this is multi class. Later will add support
 def scikit_learn_dataset_training(classifier):
     from sklearn import datasets
+
     iris = datasets.load_iris()
-    classifier.generic_task(iris.data,iris.target,'Iris')
+    classifier.generic_task(iris.data, iris.target, 'Iris')
+
 
 def class_digital_logic(classifier):
     """
@@ -286,35 +291,39 @@ def class_digital_logic(classifier):
     task_OR_problem(classifier)
     task_XOR_problem(classifier)
 
+
 # Task 1,2
 def meanie(x):
-    return np.mean(x,axis=1)
+    return np.mean(x, axis=1)
+
 
 def dot_with_11(x):
-    return np.dot(x,np.array([0.5,0.5]))
+    return np.dot(x, np.array([0.5, 0.5]))
+
 
 def simple_custom_fitting_class(classifier):
     """
-    Fit with some simple custom funcitons.
+    Fit with some simple custom functions.
     :param classifier: object SimpleClassifierBank
     :return:
     """
     # Lesson 1 11
-    classifier.fit_custom_fx(dot_with_11,2,1,'dot with [1,1]')
+    classifier.fit_custom_fx(dot_with_11, 2, 1, 'dot with [1,1]')
     # Lesson 2: Mean
-    classifier.fit_custom_fx(meanie,1500,1,'np.mean')
+    classifier.fit_custom_fx(meanie, 1500, 1, 'np.mean')
+
 
 if __name__ == '__main__':
     classifier_file_name = 'ClassifierFile.pkl'
     if os.path.isfile(classifier_file_name):
-        Main_C1 = pickle.load(open(classifier_file_name,'r'))
+        Main_C1 = pickle.load(open(classifier_file_name, 'r'))
     else:
-        Main_C1 = SimpleClassifierBank(max_width=2000,input_width=1500,height=500)
-    class_digital_logic(Main_C1)
+        Main_C1 = SimpleClassifierBank(max_width=2000, input_width=1500, height=500)
+    # class_digital_logic(Main_C1)
     # Main_C1.fit_custom_fx(np.mean,input_width=1500, output_width=1, task_name='np.mean')
-    Main_C1.predict(np.random.randn(8,22))
+    yp = Main_C1.predict(np.random.randn(8, 22))
+    print 'Predicted value is ', yp
     # Main_C1.remove_classifier('np.mean')
-    simple_custom_fitting_class(Main_C1)
+    # simple_custom_fitting_class(Main_C1)
     Main_C1.status()
-    pickle.dump(Main_C1,open(classifier_file_name,'w'))
-
+    pickle.dump(Main_C1, open(classifier_file_name, 'w'))
